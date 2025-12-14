c8b28574e0b789442e4cd37209bba4ab09023403
kageepanTK <kageepantk1122@gmail.com>
2025-12-14 07:43:55 +0530

    version2

--- Hello1.java
+++ Hello1.java
@@ -3,5 +3,6 @@ class Hello1{
     public static void main(String[] args){
     System.out.println("adittion 5+2="+ (5+2));
     System.out.print("substraction 5-2="+(5-2));
+    System.out.print("multiplication 5*2="+(5*2));
     }
 }
