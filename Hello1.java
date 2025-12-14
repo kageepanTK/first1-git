e282e8ac4c59bdfa5e58a1c87599e6f55398dd0e
kageepanTK <kageepantk1122@gmail.com>
2025-12-14 07:17:37 +0530

    version 1

---
+++ Hello1.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+class Hello1{
+    public static void main(String[] args){
+    System.out.println("adittion 5+2="+ (5+2));
+    System.out.print("substraction 5-2="+(5-2));
+    }
+}
